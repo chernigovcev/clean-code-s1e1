2f5239d534f2a3c73841d94f120279cedf46e3a3
Dmitry Chernigovcev <dmitry.chernigovcev@gmail.com>
2023-04-22 19:06:27 +0300

    fix(extended-1.2): add alt to img

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="button delete";
     deleteButtonImg.src="./remove.svg";
+    deleteButtonImg.alt="";
     deleteButton.appendChild(deleteButtonImg);
 
     deleteButtonImg.className = "remove-img";
